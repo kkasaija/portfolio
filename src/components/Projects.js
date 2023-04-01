3dee830f4f7c4669f2e5266d1448c2af1e00a9cd
Kasaija Kenneth <kasaijak@gmail.com>
2023-04-02 00:59:21 +0300

    Solve linter errors

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,16 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
 import {
   Container, Row, Col, Tab, Nav,
 } from 'react-bootstrap';
-import { ProjectCard } from './ProjectCard';
+import TrackVisibility from 'react-on-screen';
+import { uuid } from 'uuidv4';
+import ProjectCard from './ProjectCard';
 import projImg1 from '../assets/img/project-img1.png';
 import projImg2 from '../assets/img/project-img2.png';
 import projImg3 from '../assets/img/project-img3.png';
 import colorSharp2 from '../assets/img/color-sharp2.png';
 import 'animate.css';
-import TrackVisibility from 'react-on-screen';
 
 const Projects = () => {
   const projects = [
@@ -52,7 +55,12 @@ const Projects = () => {
               {({ isVisible }) => (
                 <div className={isVisible ? 'animate__animated animate__fadeIn' : ''}>
                   <h2>Projects</h2>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled
+                    it to make a type specimen book.
+                  </p>
                   <Tab.Container id="projects-tabs" defaultActiveKey="first">
                     <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                       <Nav.Item>
@@ -69,9 +77,9 @@ const Projects = () => {
                       <Tab.Pane eventKey="first">
                         <Row>
                           {
-                          projects.map((project, index) => (
+                          projects.map((project) => (
                             <ProjectCard
-                              key={index}
+                              key={uuid()}
                               {...project}
                             />
                           ))
@@ -79,10 +87,20 @@ const Projects = () => {
                         </Row>
                       </Tab.Pane>
                       <Tab.Pane eventKey="section">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam,
+                          quod neque provident velit, rem explicabo excepturi
+                          id illo molestiae blanditiis,eligendi dicta officiis asperiores
+                          delectus quasi inventore debitis quo.
+                        </p>
                       </Tab.Pane>
                       <Tab.Pane eventKey="third">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                          Cumque quam, quod neque provident velit,
+                          rem explicabo excepturi id illo molestiae blanditiis,
+                          eligendi dicta officiis asperiores delectus quasi inventore debitis quo.
+                        </p>
                       </Tab.Pane>
                     </Tab.Content>
                   </Tab.Container>
@@ -92,7 +110,7 @@ const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2} />
+      <img className="background-image-right" src={colorSharp2} alt="representation" />
     </section>
   );
 };
