f2d7f7b3814be732fc94b71172ed403da9688969
Kasaija Kenneth <kasaijak@gmail.com>
2024-06-15 10:11:20 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,9 +15,9 @@ const Footer = () => (
         <Col size={12} sm={6} className="text-center text-sm-end">
           <p>Copyright 2023. All Rights Reserved</p>
           <div className="social-icon">
-            <a href="https://github.com/kkasaija/" target="_blank"><img src={github} alt="img" /></a>
-            <a href="https://twitter.com/kenn_ug/" target="_blank"><img src={twitter} alt="img" /></a>
-            <a href="https://www.linkedin.com/in/kkasaija/" target="_blank"><img src={linkedin} alt="img" /></a>
+            <a href="https://github.com/kkasaija/" target="_blank" rel="noreferrer"><img src={github} alt="img" /></a>
+            <a href="https://twitter.com/kenn_ug/" target="_blank" rel="noreferrer"><img src={twitter} alt="img" /></a>
+            <a href="https://www.linkedin.com/in/kkasaija/" target="_blank" rel="noreferrer"><img src={linkedin} alt="img" /></a>
           </div>
         </Col>
       </Row>
